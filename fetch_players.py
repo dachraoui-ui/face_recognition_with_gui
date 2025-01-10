47193eb96da735d22ada128fa8b0a685f427dd21
ahmed dacharoui <dachraouia193@gmail.com>
2025-01-10 22:51:28 +0100

    nnnnnnnnn

--- fetch_players.py
+++ fetch_players.py
@@ -14,7 +14,7 @@ SELECT ?playerLabel ?teamLabel ?image ?birthDate ?height WHERE {
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }LIMIT 20
 """
-#another query
+
 
 
 
